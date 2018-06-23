b2c0ff1fe8525289819e4979a868736a8039c82e
Magnus Mortensen <magnusm@outlook.com>
2018-06-23 07:59:20 +0200

    Dashboard data is formatted for React Table
    But not applied to React.

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -1,7 +1,11 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
+import _ from "lodash";
 
-export default function dashboardReducer(state = initialState.aggregates, action) {
+export default function dashboardReducer(
+  state = initialState.aggregates,
+  action
+) {
   switch (action.type) {
     case types.LOAD_UPDATED_AGGREGATES_SUCCESS:
       return Object.assign([], aggregateTrackings(action.trackings));
@@ -19,7 +23,33 @@ function aggregateTrackings(trackings) {
     tracking["costPrice"] = costPrice;
     tracking["marketPrice"] = marketPrice;
     tracking["yieldValue"] = yieldValue;
-    tracking["yieldPercent"] = ((yieldValue / costPrice) * 100);
+    tracking["yieldPercent"] = (yieldValue / costPrice) * 100;
   });
+
+  var trackingsByTime = _.groupBy(allTrackings, "trackingTime");
+  //console.log(JSON.stringify(trackingsByTime));
+
+  let final = [];
+
+  for (var time in trackingsByTime) {
+    let totalCostPrice = 0;
+    let totalMarketPrice = 0;
+    let totalYieldValue = 0;
+    trackingsByTime[time].forEach(tracking => {
+      totalCostPrice = totalCostPrice + tracking.costPrice;
+      totalMarketPrice = totalMarketPrice + tracking.marketPrice;
+      totalYieldValue = totalYieldValue + tracking.yieldValue;
+    });
+    final.push({
+      "trackingTime": time,
+      "costPrice": totalCostPrice,
+      "marketPrice": totalMarketPrice,
+      "yieldValue": totalYieldValue,
+      "yieldPercent": (totalYieldValue / totalCostPrice) * 100,
+      "children": trackingsByTime[time]
+    });
+  }
+  console.log(JSON.stringify(final));
+
   return allTrackings;
 }
