b13fca3b64589bdef61d414a2dd2b9a5a357039e
Magnus Mortensen <magnusm@outlook.com>
2018-06-29 20:41:33 +0200

    Adding to dashboard table.

--- src/components/dashboard/DashboardTable.js
+++ src/components/dashboard/DashboardTable.js
@@ -2,54 +2,63 @@ import React from "react";
 import PropTypes from "prop-types";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
-import _ from 'lodash'
+import _ from "lodash";
+import { getFormattedDate } from "../../utils/dateHelper";
 
 const DashboardTable = ({ rows, assets }) => {
   return (
     <div>
-    <ReactTable
-      data={rows}
-      columns={[
-        {
-          Header: "Time and Assets",
-          columns: [
-            {
-              Header: "Tracking Time",
-              accessor: "trackingTime"
+      <ReactTable
+        data={rows}
+        columns={[
+          {
+            Header: "Tracking Time",
+            id: "trackingTime",
+            accessor: d => {
+              let theDate = getFormattedDate(d.trackingTime);
+              if (theDate.length) return theDate;
             },
-            {
-              Header: "Asset",
-              id: "assetId",
-              accessor: d => { let asset = assets.filter(asset => asset.value == d.assetId); if(asset.length) return asset[0].text; }
-            }
-          ]
-        },
-        {
-          Header: "Price and Yield",
-          columns: [
-            {
-              Header: "Cost Price",
-              accessor: "costPrice",
-              aggregate: vals => _.sum(vals)
+            width: 100
+          },
+          {
+            Header: "Asset",
+            id: "assetId",
+            accessor: d => {
+              let asset = assets.filter(asset => asset.value == d.assetId);
+              if (asset.length) return asset[0].text;
             },
-            {
-              Header: "Market Price",
-              accessor: "marketPrice",
-              aggregate: vals => _.sum(vals)
-            },
-            {
-              Header: "Yield Value",
-              accessor: "yieldValue",
-              aggregate: vals => _.sum(vals)
-            }
-          ]
-        }
-      ]}
-      pivotBy={["trackingTime"]}
-      defaultPageSize={10}
-      className="-striped -highlight"
-    />
-  </div>
+            width: 500
+          },
+          {
+            Header: "Cost Price",
+            accessor: "costPrice",
+            aggregate: vals => _.sum(vals),
+            width: 100
+          },
+          {
+            Header: "Market Price",
+            accessor: "marketPrice",
+            aggregate: vals => _.sum(vals),
+            width: 100
+          },
+          {
+            Header: "Yield",
+            accessor: "yieldValue",
+            aggregate: vals => _.sum(vals),
+            width: 100
+          },
+          {
+            Header: "Yield Percent",
+            accessor: "yieldPercent",
+            aggregate: vals => _.mean(vals),
+            width: 100
+          }
+        ]}
+        pivotBy={["trackingTime"]}
+        defaultPageSize={10}
+        className="-striped -highlight"
+      />
+    </div>
   );
 };
 
